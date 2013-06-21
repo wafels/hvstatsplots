772f02fcca2681e6aa3342afc186cb00f977e59e
Jack Ireland <jackireland@gmail.com>
2013-06-21 17:09:20 -0400

    improved plots

--- monthly_usage.py
+++ monthly_usage.py
@@ -2,20 +2,48 @@
 
 import pandas
 import os
+from matplotlib import pyplot as plt
+plt.ioff()
 #import style_sheet as ss
 
-dir = os.path.expanduser('~/hv/txt')
+dir = os.path.expanduser('~/hv/txt/spd/')
 
 # which files to load
 
 files = ['screenshot', 'hv', 'jhv']
+legends = {"screenshot": 'screenshots (Helioviewer.org)',
+           "hv": 'movies (Helioviewer.org)',
+           "jhv": 'JPX requests (JHelioviewer)'}
 
 # load in the data to a data frame
 d={}
 for f in files:
-    d[f] = pandas.Series.from_csv(''.join(dir,f,'.txt'))
+    d[f] = pandas.Series.from_csv(''.join((dir,f,'.txt')))
 
-df = pandas.DataFrame(d)
+# Raw numbers
+numbers_df = pandas.DataFrame(d)
+numbers_total = numbers_df.sum(axis=1)
+numbers_df.rename(columns=legends, inplace=True)
+
+plt.figure(1)
+numbers_df.plot(lw=5)
+plt.ylabel('number')
+plt.title('Monthly Usage By Service')
+
+
+# Percentages
+dp = {}
+for f in files:
+    dp[f] = 100*d[f] / numbers_total
+percent_df= pandas.DataFrame(dp)
+percent_df.rename(columns=legends, inplace=True)
+
+plt.figure(2)
+percent_df.plot(lw=5)
+plt.ylabel('percentage')
+plt.title('Percentage Monthly Usage By Service')
+plt.show()
+
 
 # make it a nice plot...
 
